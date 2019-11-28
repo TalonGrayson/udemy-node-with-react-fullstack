461e2c359c835626cbff09ec18949de59a3042fa
MatrixStefan <stefan.ritchie@thisismatrix.com>
2019-11-28 18:28:03 +0000

    initial commit

---
+++ index.js
@@ -0,0 +1,17 @@
+const express = require('express')
+const app = express();
+
+app.get('/', (req, res) => {
+    res.send({hi: 'there'});
+});
+
+app.get('/bye*', (req, res) => {
+    res.send({bye: 'there'});
+});
+
+app.get('/*', (req, res) => {
+    res.send({hi: 'here'});
+});
+
+const PORT = process.env.PORT || 5000
+app.listen(PORT);
